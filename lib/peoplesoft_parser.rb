254f0d713d781902cd48c83c7938a25d3266287b
Ben Meier <benmeier42@gmail.com>
2014-08-26 21:31:40 +0200

    Added method to construct objects out of course rows

--- lib/peoplesoft_parser.rb
+++ lib/peoplesoft_parser.rb
@@ -6,6 +6,9 @@ class PeoplesoftParser
     PEOPLESOFT_URI = URI 'https://srvslspsw001.uct.ac.za/psp/public/EMPLOYEE/HRMS/c/UCT_PUBLIC_MENU.UCT_SS_ADV_PUBLIC.GBL?FolderPath=PORTAL_ROOT_OBJECT.UCT_SS_ADV_PUBLIC_GBL_1&IsFolder=false&IgnoreParamTempl=FolderPath%252cIsFolder'
     REDIRECT_LIMIT = 10
 
+    NOT_FOUND_TEXT = 'No student record found for this Campus ID'
+    FAIL_TEXT = ''
+
     def initialize
         @cookies = ''
     end
@@ -23,16 +26,16 @@ class PeoplesoftParser
 
             response = http.request_post(URI('https://srvslspsw001.uct.ac.za/psc/public/EMPLOYEE/HRMS/c/UCT_PUBLIC_MENU.UCT_SS_ADV_PUBLIC.GBL'), construct_post_data(student_number, icsid), {'Cookie' => @cookies})
 
-            doc = Nokogiri::HTML(response.body)
+            if response.body.include? NOT_FOUND_TEXT
+                return nil
+            else
+                open('dump3.txt', "w") { |io| io.write(response.body) }
 
-            rows = doc.xpath("//table[@id='TERM_CLASSES$scroll$0']/tr/td/table/tr[position()>1]")
+                doc = Nokogiri::HTML(response.body)
+                rows = doc.xpath("//table[@id='TERM_CLASSES$scroll$0']/tr/td/table/tr[position()>1]")
 
-            rows.each do |r|
-                puts r.css('span').map { |s| s.content }
+                return rows.map { |r| construct_course_model(r.css('span'))  }
             end
-
-            open('dump3.txt', "w") { |io| io.write(response.body) }
-
         end
     end
 
@@ -57,8 +60,47 @@ class PeoplesoftParser
             end
         end
 
-        def construct_post_data(sn, sid)
-            "ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum=1&ICAction=UCT_DERIVED_PUB_SS_DERIVED_LINK&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=0&ICSID=#{sid}&ICActionPrompt=false&ICFind=&ICAddCount=&ICAPPCLSDATA=&UCT_DERIVED_PUB_CAMPUS_ID=#{sn}&UCT_DERIVED_PUB_UCT_DERIVED_LINK6=GI"
+        def construct_course_model(span_array)
+            {
+                course: span_array[0].content,
+                description: span_array[1].content,
+                units: span_array[2].content,
+                grading: span_array[3].content,
+                grade: span_array[4].content,
+                points: span_array[5].content
+            }
+        end
+
+        def construct_post_data(student_number, sid)
+            """
+            Construct the data string that will be POSTED to complete the form.
+            This must contain the ID from the previous page, and the student_number
+            to lookup.
+            """
+            URI.encode_www_form({
+                'ICAJAX' => 1,
+                'ICNAVTYPEDROPDOWN' => 0,
+                'ICType' => 'Panel',
+                'ICElementNum' => 0,
+                'ICStateNum' => 1,
+                'ICAction' => 'UCT_DERIVED_PUB_SS_DERIVED_LINK',
+                'ICXPos' => 0,
+                'ICYPos' => 0,
+                'ResponsetoDiffFrame' => -1,
+                'TargetFrameName' => 'None',
+                'FacetPath' => 'None',
+                'ICFocus' => '',
+                'ICSaveWarningFilter' => '',
+                'ICChanged' => -1,
+                'ICResubmit' => 0,
+                'ICActionPrompt' => false,
+                'ICFind' => '',
+                'ICAddCount' => '',
+                'ICAPPCLSDATA' => '',
+                'UCT_DERIVED_PUB_UCT_DERIVED_LINK6' => 'GI',
+                'UCT_DERIVED_PUB_CAMPUS_ID' => student_number.upcase,
+                'ICSID' => sid
+            })
         end
 
 end
--- spec/highlevel/highlevel_spec.rb
+++ spec/highlevel/highlevel_spec.rb
@@ -1,5 +1,7 @@
 describe PeoplesoftParser do
     it 'can retrieve student marks' do
         marks = PeoplesoftParser.new.retrieve('ODNSIO001')
+        puts marks
+        expect(1).to eq(2)
     end
 end
